fca03d1679b67cd2ee1d389239de5a45fd56837f
Mack <hypermail@yandex.ru>
2024-11-17 19:12:02 +0300

    current_url тест

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.url == 'http://selenium1py.pythonanywhere.com/', 'Неверный URL адрес'
+        self.browser.find_element(*LoginPageLocators.LOGIN_LINK).click()
+        assert self.browser.current_url == 'https://selenium1py.pythonanywhere.com/en-gb/accounts/login/', 'Неверный URL адрес'
 
     def should_be_login_form(self):
         self.browser.find_element(*LoginPageLocators.LOGIN_LINK).click()
